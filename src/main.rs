df723196c7e6a4c6957df9a2125caf681aebb5f7
Jeremy Soller <jeremy@system76.com>
2021-10-13 15:27:21 -0600

    Only run on HP devices, only run as root

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use os_release::OsRelease;
+use std::process;
 
 use report::{Report, ReportFreq, report_file};
 mod report;
 
 fn main() {
+    if unsafe { libc::geteuid() } != 0 {
+        eprintln!("hp-vendor: must be run as root");
+        process::exit(1);
+    }
+
+    let sys_vendor = report_file("/sys/class/dmi/id/sys_vendor").unwrap_or_else(|_| {
+        "Unknown".to_string()
+    });
+    if sys_vendor != "HP" {
+        eprintln!("hp-vendor: unknown vendor '{}'", sys_vendor);
+        process::exit(1);
+    }
+
     let mut report = Report::new();
 
     {
@@ -11,11 +25,23 @@ fn main() {
         section.item("Vendor", ReportFreq::Boot, || {
             report_file("/sys/class/dmi/id/sys_vendor")
         });
+        section.item("Family", ReportFreq::Boot, || {
+            report_file("/sys/class/dmi/id/product_family")
+        });
         section.item("Name", ReportFreq::Boot, || {
             report_file("/sys/class/dmi/id/product_name")
         });
+        section.item("Serial", ReportFreq::Boot, || {
+            report_file("/sys/class/dmi/id/product_serial")
+        });
+        section.item("SKU", ReportFreq::Boot, || {
+            report_file("/sys/class/dmi/id/product_sku")
+        });
         section.item("Version", ReportFreq::Boot, || {
             report_file("/sys/class/dmi/id/product_version")
+        });
+        section.item("UUID", ReportFreq::Boot, || {
+            report_file("/sys/class/dmi/id/product_uuid")
         });
     }
 
@@ -26,6 +52,9 @@ fn main() {
         });
         section.item("Name", ReportFreq::Boot, || {
             report_file("/sys/class/dmi/id/board_name")
+        });
+        section.item("Serial", ReportFreq::Boot, || {
+            report_file("/sys/class/dmi/id/board_serial")
         });
         section.item("Version", ReportFreq::Boot, || {
             report_file("/sys/class/dmi/id/board_version")
@@ -49,6 +78,9 @@ fn main() {
         let section = report.section("Chassis");
         section.item("Vendor", ReportFreq::Boot, || {
             report_file("/sys/class/dmi/id/chassis_vendor")
+        });
+        section.item("Serial", ReportFreq::Boot, || {
+            report_file("/sys/class/dmi/id/chassis_serial")
         });
         section.item("Version", ReportFreq::Boot, || {
             report_file("/sys/class/dmi/id/chassis_version")
