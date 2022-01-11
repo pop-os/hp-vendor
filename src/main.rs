95b1f8e883a0f3f6052350f7b56b913654f82c16
Ian Douglas Scott <idscott@system76.com>
2022-01-11 11:51:05 -0800

    Abstraction for mapping report types to reports

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,11 @@
-use nix::sys::utsname::uname;
 use os_release::OsRelease;
 use raw_cpuid::CpuId;
 use std::{io, process};
-use time::{format_description, OffsetDateTime};
 
-mod event;
 use report::{report_file, Report, ReportFreq};
 mod report;
 
-fn unknown() -> String {
-    "unknown".to_string()
-}
-
-fn data_header() -> event::TelemetryHeaderModel {
-    let (os_name, os_version) = match OsRelease::new() {
-        Ok(OsRelease { name, version, .. }) => (name, version),
-        Err(_) => (unknown(), unknown()),
-    };
-
-    // XXX offset format? Fraction?
-    let format =
-        format_description::parse("[year]-[month]-[day]T[hour]:[minute]:[second]").unwrap();
-    let (timestamp, timestamp_utc_offset) = match OffsetDateTime::now_local() {
-        Ok(time) => (
-            time.format(&format).ok().unwrap_or_else(unknown),
-            time.offset().whole_hours().into(),
-        ),
-        Err(_) => (unknown(), 0),
-    };
-
-    event::TelemetryHeaderModel {
-        consent: event::DataCollectionConsent {
-            level: String::new(), // TODO
-        },
-        data_provider: event::DataProviderInfo {
-            app_name: env!("CARGO_PKG_NAME").to_string(),
-            app_version: env!("CARGO_PKG_VERSION").to_string(),
-            os_name,
-            os_version,
-        },
-        ids: event::DeviceOSIds {
-            bios_uuid: String::new(),     // TODO
-            device_id: String::new(),     // TODO
-            os_install_id: String::new(), // TODO
-        },
-        timestamp,
-        timestamp_utc_offset,
-    }
-}
+use hp_vendor::event;
 
 fn main() {
     if unsafe { libc::geteuid() } != 0 {
@@ -183,20 +141,14 @@ fn main() {
 
     println!("{:#?}", report.values());
 
-    let utsname = uname();
     println!(
         "{}",
-        serde_json::to_string_pretty(&event::Event {
-            data_header: data_header(),
-            data: vec![event::AnyTelemetryEventEnum::SwLinuxKernel(
-                event::LinuxKernel {
-                    name: utsname.sysname().to_string(),
-                    release: utsname.release().to_string(),
-                    state: event::Swstate::Same, // TODO
-                    version: utsname.version().to_string(),
-                }
-            )]
-        })
-        .unwrap()
+        event::Event {
+            data_header: hp_vendor::data_header(),
+            data: event::TelemetryEventType::iter()
+                .filter_map(hp_vendor::event)
+                .collect(),
+        }
+        .to_json_pretty()
     );
 }
