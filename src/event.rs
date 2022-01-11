168bf1ce8e6a06284f64b2f070807754b7d9a6d0
Ian Douglas Scott <idscott@system76.com>
2022-01-10 16:31:37 -0800

    Generate event enum

--- src/event.rs
+++ src/event.rs
@@ -3,35 +3,7 @@ use serde::{Deserialize, Serialize};
 schemafy::schemafy!("event_package.json");
 
 // Unlike genereated binding, an `enum` rather than a `struct`
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "snake_case")]
-pub enum AnyTelemetryEventEnum {
-    HwBaseBoard(BaseBoard),
-    HwBattery(Battery),
-    HwBatteryUsage(BatteryUsage),
-    HwDisplay(Display),
-    HwGraphicsCard(GraphicsCard),
-    HwMemoryPhysical(MemoryPhysical),
-    HwNetworkCard(NetworkCard),
-    HwNvmeSmartLog(NvmesmartLog),
-    HwNvmeStorageLogical(NvmestorageLogical),
-    HwNvmeStoragePhysical(NvmestoragePhysical),
-    HwPeripheralAudioPort(PeripheralAudioPort),
-    HwPeripheralHdmi(PeripheralHDMI),
-    HwPeripheralSimCard(PeripheralSIMCard),
-    HwPeripheralUsbTypeA(PeripheralUSBTypeA),
-    HwPeripheralUsbTypeC(PeripheralUSBTypeC),
-    HwPeripheralUsbTypeCDisplayPort(PeripheralUSBTypeCDisplayPort),
-    HwProcessor(Processor),
-    HwSystem(System),
-    HwThermalContext(ThermalContext),
-    HwTpm(TrustedPlatformModule),
-    SwDriver(Driver),
-    SwFirmware(Firmware),
-    SwLinuxDriverCrash(LinuxDriverCrash),
-    SwLinuxKernel(LinuxKernel),
-    SwOperatingSystem(OperatingSystem),
-}
+include!(concat!(env!("OUT_DIR"), "/event_enum.rs"));
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Event {
