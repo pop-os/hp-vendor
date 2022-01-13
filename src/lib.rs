01fae61a2b5be15788f8d0fda0a306b03b2c3ea4
Ian Douglas Scott <idscott@system76.com>
2022-01-13 11:52:29 -0800

    Generate more events

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 use nix::sys::utsname::uname;
 use os_release::OsRelease;
-use time::{format_description, OffsetDateTime};
+use std::{fs, path::Path, str::FromStr};
+use time::OffsetDateTime;
 
 pub mod event;
 pub mod report;
 
 use event::{AnyTelemetryEventEnum, TelemetryEventType};
 use report::ReportFreq;
+
+pub fn read_file<P: AsRef<Path>, T: FromStr>(path: P) -> Option<T> {
+    fs::read_to_string(path).ok().and_then(|x| x.parse().ok())
+}
 
 pub struct EventDesc {
     freq: ReportFreq,
@@ -73,6 +78,78 @@ pub fn event(type_: TelemetryEventType) -> Option<EventDesc> {
                 release: utsname.release().to_string(),
                 state: event::Swstate::Same, // TODO
                 version: utsname.version().to_string(),
+            }
+            .into()
+        }),
+        TelemetryEventType::HwBattery => EventDesc::new(ReportFreq::Daily, || {
+            event::Battery {
+                ct_number: String::new(), // XXX,
+                devicename: read_file("/sys/class/power_supply/BAT0/model_name"),
+                energy_design: read_file("/sys/class/power_supply/BAT0/charge_full_design")
+                    .map(|x: f64| x / 1000000.), // XXX divisor?
+                manufacturer: read_file("/sys/class/power_supply/BAT0/manufacturer"),
+                serial_number: read_file("/sys/class/power_supply/BAT0/serial_number")
+                    .unwrap_or_else(unknown),
+                state: event::Hwstate::Same, // TODO
+                voltage_design: read_file("/sys/class/power_supply/BAT0/voltage_min_design")
+                    .map(|x: f64| x / 1000.),
+            }
+            .into()
+        }),
+        TelemetryEventType::HwBaseBoard => EventDesc::new(ReportFreq::Daily, || {
+            event::BaseBoard {
+                base_board_id: read_file("/sys/class/dmi/id/board_name"),
+                ct_number: String::new(), // XXX
+                manufacturer: read_file("/sys/class/dmi/id/board_vendor"),
+                state: event::Hwstate::Same, // TODO
+                version: read_file("/sys/class/dmi/id/board_version"),
+            }
+            .into()
+        }),
+        TelemetryEventType::SwFirmware => EventDesc::new(ReportFreq::Daily, || {
+            event::Firmware {
+                address: None, // XXX
+                bios_release_date: read_file("/sys/class/dmi/id/bios_date"),
+                bios_vendor: read_file("/sys/class/dmi/id/bios_vendor"),
+                bios_version: read_file("/sys/class/dmi/id/bios_version"),
+                capabilities: None, // XXX
+                embedded_controller_version: read_file("/sys/class/dmi/id/ec_firmware_release"),
+                rom_size: None,              // XXX
+                runtime_size: None,          // XXX
+                smbios_version: None,        // XXX
+                state: event::Swstate::Same, // XXX
+            }
+            .into()
+        }),
+        TelemetryEventType::HwSystem => EventDesc::new(ReportFreq::Daily, || {
+            event::System {
+                capabilities: None, // XXX
+                chassis: read_file("/sys/class/dmi/id/chassis_type"),
+                family: read_file("/sys/class/dmi/id/product_family"),
+                feature_byte: None, // XXX
+                manufacturer: read_file("/sys/class/dmi/id/sys_vendor"),
+                model: read_file("/sys/class/dmi/id/product_name"),
+                serialnumber: read_file("/sys/class/dmi/id/product_serial").unwrap_or_else(unknown),
+                sku: read_file("/sys/class/dmi/id/product_sku"),
+                state: event::Hwstate::Same, // XXX,
+                uuid: read_file("/sys/class/dmi/id/product_uuid").unwrap_or_else(unknown),
+                version: read_file("/sys/class/dmi/id/product_version"),
+                width: None, // XXX
+            }
+            .into()
+        }),
+        TelemetryEventType::SwOperatingSystem => EventDesc::new(ReportFreq::Daily, || {
+            let os_release = OsRelease::new().ok();
+            event::OperatingSystem {
+                boot_device: String::new(), // XXX
+                codename: os_release.as_ref().map(|x| x.version_codename.to_owned()),
+                manufacturer: None, // XXX
+                name: os_release
+                    .as_ref()
+                    .map_or_else(unknown, |x| x.name.to_owned()),
+                sku: None,                   // XXX
+                state: event::Swstate::Same, // XXX
+                version: os_release.map(|x| x.version.clone()),
             }
             .into()
         }),
