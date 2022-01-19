bbabe5fd1f23bb8e3bfd8bae11dc2b8a17fe102b
Ian Douglas Scott <idscott@system76.com>
2022-01-19 13:07:57 -0800

    Remove old report code
    Implemented with new Json schema, except processor info. Though this
    schema doesn't include things like charge cycles.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use os_release::OsRelease;
 use raw_cpuid::CpuId;
-use std::{io, process};
+use std::{fs, process};
 
-use hp_vendor::{
-    event,
-    report::{report_file, Report, ReportFreq},
-};
+use hp_vendor::event;
 
 fn main() {
     if unsafe { libc::geteuid() } != 0 {
@@ -13,134 +9,12 @@ fn main() {
         process::exit(1);
     }
 
-    let product_name =
-        report_file("/sys/class/dmi/id/product_name").unwrap_or_else(|_| "Unknown".to_string());
+    let product_name = fs::read_to_string("/sys/class/dmi/id/product_name").unwrap();
     #[cfg(not(feature = "disable-model-check"))]
     if product_name != "HP EliteBook 845 G8 Notebook PC" {
         eprintln!("hp-vendor: unknown product '{}'", product_name);
         process::exit(1);
     }
-
-    let mut report = Report::new();
-
-    {
-        let section = report.section("System");
-        section.item("Vendor", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/sys_vendor")
-        });
-        section.item("Family", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/product_family")
-        });
-        section.item("Name", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/product_name")
-        });
-        section.item("Serial", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/product_serial")
-        });
-        section.item("SKU", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/product_sku")
-        });
-        section.item("Version", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/product_version")
-        });
-        section.item("UUID", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/product_uuid")
-        });
-    }
-
-    {
-        let section = report.section("Base Board");
-        section.item("Vendor", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/board_vendor")
-        });
-        section.item("Name", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/board_name")
-        });
-        section.item("Serial", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/board_serial")
-        });
-        section.item("Version", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/board_version")
-        });
-    }
-
-    {
-        let section = report.section("Firmware");
-        section.item("Vendor", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/bios_vendor")
-        });
-        section.item("Version", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/bios_version")
-        });
-        section.item("Release Date", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/bios_date")
-        });
-    }
-
-    {
-        let section = report.section("Chassis");
-        section.item("Vendor", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/chassis_vendor")
-        });
-        section.item("Serial", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/chassis_serial")
-        });
-        section.item("Version", ReportFreq::Boot, || {
-            report_file("/sys/class/dmi/id/chassis_version")
-        });
-    }
-
-    {
-        let section = report.section("Operating System");
-        section.item("Name", ReportFreq::Boot, || {
-            OsRelease::new().map(|x| x.name)
-        });
-        section.item("Version", ReportFreq::Boot, || {
-            OsRelease::new().map(|x| x.version)
-        });
-    }
-
-    {
-        let section = report.section("Battery");
-        section.item("Capacity", ReportFreq::Boot, || {
-            report_file("/sys/class/power_supply/BAT0/capacity")
-        });
-        section.item("Cycle Count", ReportFreq::Boot, || {
-            report_file("/sys/class/power_supply/BAT0/cycle_count")
-        });
-        section.item("Manufacturer", ReportFreq::Boot, || {
-            report_file("/sys/class/power_supply/BAT0/manufacturer")
-        });
-        section.item("Model", ReportFreq::Boot, || {
-            report_file("/sys/class/power_supply/BAT0/model_name")
-        });
-        section.item("Serial", ReportFreq::Boot, || {
-            report_file("/sys/class/power_supply/BAT0/serial_number")
-        });
-        section.item("Technology", ReportFreq::Boot, || {
-            report_file("/sys/class/power_supply/BAT0/technology")
-        });
-    }
-
-    {
-        let section = report.section("CPU");
-        section.item("Vendor", ReportFreq::Boot, || {
-            CpuId::new()
-                .get_vendor_info()
-                .map(|x| x.as_str().to_string())
-                .ok_or(io::Error::new(io::ErrorKind::NotFound, "no cpuid vendor"))
-        });
-        section.item("Model", ReportFreq::Boot, || {
-            CpuId::new()
-                .get_processor_brand_string()
-                .map(|x| x.as_str().to_string())
-                .ok_or(io::Error::new(io::ErrorKind::NotFound, "no cpuid model"))
-        });
-    }
-
-    report.update();
-
-    println!("{:#?}", report.values());
 
     println!(
         "{}",
