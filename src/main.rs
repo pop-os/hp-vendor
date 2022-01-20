2d717e3ac6c46929db0faa850cf772d2090a931c
Ian Douglas Scott <idscott@system76.com>
2022-01-19 20:05:41 -0800

    Fix `product_name` handling... again

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() {
 
     #[cfg(not(feature = "disable-model-check"))]
     {
-        let product_name = std::fs::read_to_string("/sys/class/dmi/id/product_name")
-            .unwrap_or_else(|_| "unknown".to_string());
+        let product_name = std::fs::read_to_string("/sys/class/dmi/id/product_name").ok();
+        let product_name = product_name.as_deref().unwrap_or("unknown").trim();
         if product_name != "HP EliteBook 845 G8 Notebook PC" {
             eprintln!("hp-vendor: unknown product '{}'", product_name);
             process::exit(1);
