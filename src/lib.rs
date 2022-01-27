34ba01b05c26ba40650946d3385a25e461773e05
Ian Douglas Scott <idscott@system76.com>
2022-01-27 09:06:02 -0800

    Add function to get PCIe DNS

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use nix::sys::utsname::uname;
 use os_release::OsRelease;
-use std::{fs, str::FromStr};
+
+use std::{collections::HashSet, fmt::Write, fs, path::Path, str::FromStr};
 use time::{format_description::well_known::Rfc3339, OffsetDateTime};
 
 pub mod api;
@@ -10,12 +11,40 @@ pub mod report;
 use event::{read_file, unknown, TelemetryEvent, TelemetryEventType};
 use report::ReportFreq;
 
+const PCI_EXT_CAP_ID_DSN: u16 = 0x03;
+
 fn dmi() -> Vec<dmi::Table> {
     if let Ok(data) = fs::read("/sys/firmware/dmi/tables/DMI") {
         dmi::tables(&data)
     } else {
         Vec::new()
     }
+}
+
+pub fn pcie_dsn<P: AsRef<Path>>(path: P) -> Option<String> {
+    let data = std::fs::read(path.as_ref()).ok()?;
+    let mut been = HashSet::new();
+    let mut offset = 0x100;
+    while offset != 0 && been.insert(offset) {
+        macro_rules! entry {
+            ($offset:expr) => {
+                *data.get(offset + $offset)?
+            };
+        }
+        let next = ((entry![3] as u16) << 4) | ((entry![2] as u16) >> 4);
+        let _version = entry![2] & 0xf;
+        let cap_id = entry![0] as u16 | ((entry![1] as u16) << 8);
+        if cap_id == PCI_EXT_CAP_ID_DSN {
+            let mut dsn = String::new();
+            for i in data.get(offset as usize + 4..offset as usize + 12)? {
+                write!(dsn, "{:02x}-", i).ok()?;
+            }
+            dsn.pop();
+            return Some(dsn);
+        }
+        offset = next.into();
+    }
+    None
 }
 
 pub struct EventDesc {
