6916cae1dc81aa13976783b4cf3d57cff8cab324
Ian Douglas Scott <idscott@system76.com>
2022-02-08 15:55:41 -0800

    Unlock/wait utility functions

--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,48 @@
-use nix::fcntl::{fcntl, FcntlArg};
+use nix::{
+    errno::Errno,
+    fcntl::{fcntl, FcntlArg},
+};
 use std::{fs, os::unix::io::AsRawFd};
 
-/// Lock whole file. Returns `EACCESS` or `EAGAIN` if already locked.
+/// Set unique advisory lock on whole file Returns `EACCESS` or `EAGAIN` if
+/// already locked.
 pub fn setlk(file: &fs::File) -> nix::Result<()> {
     fcntl(
         file.as_raw_fd(),
         FcntlArg::F_SETLK(&libc::flock {
             l_type: libc::F_WRLCK as _,
+            l_whence: libc::SEEK_SET as _,
+            l_start: 0,
+            l_len: 0,
+            l_pid: 0,
+        }),
+    )
+    .map(|_| ())
+}
+
+pub fn setlk_wait(file: &fs::File) -> nix::Result<()> {
+    loop {
+        let res = fcntl(
+            file.as_raw_fd(),
+            FcntlArg::F_SETLKW(&libc::flock {
+                l_type: libc::F_WRLCK as _,
+                l_whence: libc::SEEK_SET as _,
+                l_start: 0,
+                l_len: 0,
+                l_pid: 0,
+            }),
+        );
+        if res != Err(Errno::EINTR) {
+            return res.map(|_| ());
+        }
+    }
+}
+
+pub fn unlck(file: &fs::File) -> nix::Result<()> {
+    fcntl(
+        file.as_raw_fd(),
+        FcntlArg::F_SETLK(&libc::flock {
+            l_type: libc::F_UNLCK as _,
             l_whence: libc::SEEK_SET as _,
             l_start: 0,
             l_len: 0,
