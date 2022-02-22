8822369faf82e5fb166767c59185c88006a63bfa
Ian Douglas Scott <idscott@system76.com>
2022-02-22 10:09:58 -0800

    Move (currently unused) pcie code to `util`

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@ use nix::sys::utsname::uname;
 use os_release::OS_RELEASE;
 use plain::Plain;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::HashMap,
     ffi::OsStr,
-    fmt::Write,
     fs, io,
     path::{Path, PathBuf},
     process::Command,
@@ -26,34 +25,6 @@ use util::{
     dmi::{dmi, CacheInfo21},
     drm::DrmDevice,
 };
-
-const PCI_EXT_CAP_ID_DSN: u16 = 0x03;
-
-pub fn pcie_dsn<P: AsRef<Path>>(path: P) -> Option<String> {
-    let data = std::fs::read(path.as_ref()).ok()?;
-    let mut been = HashSet::new();
-    let mut offset = 0x100;
-    while offset != 0 && been.insert(offset) {
-        macro_rules! entry {
-            ($offset:expr) => {
-                *data.get(offset + $offset)?
-            };
-        }
-        let next = ((entry![3] as u16) << 4) | ((entry![2] as u16) >> 4);
-        let _version = entry![2] & 0xf;
-        let cap_id = entry![0] as u16 | ((entry![1] as u16) << 8);
-        if cap_id == PCI_EXT_CAP_ID_DSN {
-            let mut dsn = String::new();
-            for i in data.get(offset as usize + 4..offset as usize + 12)? {
-                write!(dsn, "{:02x}-", i).ok()?;
-            }
-            dsn.pop();
-            return Some(dsn);
-        }
-        offset = next.into();
-    }
-    None
-}
 
 fn battery() -> Option<PathBuf> {
     for entry in fs::read_dir("/sys/class/power_supply").ok()? {
