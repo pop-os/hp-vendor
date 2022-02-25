8f4243e2fd480286182823e9c343747c747b7539
Ian Douglas Scott <idscott@system76.com>
2022-02-25 12:48:00 -0800

    Run only on recognized hardware

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -19,17 +19,10 @@ pub fn run() {
         process::exit(1);
     }
 
-    /*
-    #[cfg(not(feature = "disable-model-check"))]
-    {
-        let product_name = std::fs::read_to_string("/sys/class/dmi/id/product_name").ok();
-        let product_name = product_name.as_deref().unwrap_or("unknown").trim();
-        if product_name != "HP EliteBook 845 G8 Notebook PC" {
-            eprintln!("hp-vendor: unknown product '{}'", product_name);
-            process::exit(1);
-        }
+    if let Err(err) = crate::supported_hardware() {
+        eprintln!("Unsupported hardware: {}", err);
+        process::exit(1);
     }
-    */
 
     if let Err(err) = create_var_dir() {
         if err.kind() != io::ErrorKind::AlreadyExists {
