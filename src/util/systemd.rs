a88a4ae33fdab33affca84f2c27199ac01bb4b30
Ian Douglas Scott <idscott@system76.com>
2022-03-26 20:06:54 -0700

    Add util functions to enable/disable service

--- src/util/systemd.rs
+++ src/util/systemd.rs
@@ -2,11 +2,38 @@
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
+#![allow(dead_code)]
+
 use std::process::Command;
+
+const SERVICE: &str = "hp-vendor.service";
+const SERVICES_AND_TIMERS: &[&str] = &[SERVICE, "hp-vendor-daily.timer", "hp-vendor-upload.timer"];
 
 /// Restarts daemon if running, to handle frequencies change
 pub fn try_restart_daemon() {
     let _ = Command::new("systemctl")
-        .args(&["try-restart", "hp-vendor"])
+        .args(&["try-restart", SERVICE])
+        .status();
+}
+
+pub fn enable_services_and_timers() {
+    let _ = Command::new("systemctl")
+        .arg("enable")
+        .args(SERVICES_AND_TIMERS)
+        .status();
+    let _ = Command::new("systemctl")
+        .arg("start")
+        .args(SERVICES_AND_TIMERS)
+        .status();
+}
+
+pub fn disable_services_and_timers() {
+    let _ = Command::new("systemctl")
+        .arg("stop")
+        .args(SERVICES_AND_TIMERS)
+        .status();
+    let _ = Command::new("systemctl")
+        .arg("disable")
+        .args(SERVICES_AND_TIMERS)
         .status();
 }
